0e43d508de3d75484153831689c4d9a65d08efb6
Minh Hieu <oppahd96@gmail.com>
2023-02-06 19:46:18 +0700

    update style

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -51,7 +51,7 @@ const Projects = () => {
           <CardInfo>{project.descriptionJP}</CardInfo>
           :<CardInfo>{project.descriptionVN}</CardInfo>
           :null}
-          <div>
+          <div style={{marginTop: 10}}>
             <TitleContent>{typeof window !== 'undefined' ? localStorage.getItem('lang')==='eng'?'STACK':localStorage.getItem('lang')==='jpn'?'スタック':'STACK':null}</TitleContent>
             <TagList>
               {project.tags.map((tag,i)=>(
